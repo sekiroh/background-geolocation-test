6c4f7c3301544b20e334fdf1512bd1b5a6d5cb60
vincent-spies <vincent.spies@hers.be>
2016-08-10 17:05:41 +0200

    Problem: gps time

--- www/js/app.js
+++ www/js/app.js
@@ -24,6 +24,7 @@
     $this.locations = [];
     $this.isRunning = false;
     $this.locationActivated = true;
+    $this.time = "";
 
     $ionicPlatform.ready(function() {
 
@@ -44,10 +45,25 @@
       };
 
       $this.startGps = function() {
+
+        // Geolocation from cordova-plugin-geolocation
+        var success1 = function(location) {
+          console.log("Success location from cordova-plugin");
+          console.log(location);
+          $scope.$apply(function() {
+            $this.time = location.timestamp;
+          });
+        };
+        var err1 = function(err) { console.warn(err); };
+        navigator.geolocation.getCurrentPosition(success1, err1);
+
+
+        // Geolocation from cordova-mauron85-background-plugin-geolocation
         $this.isRunning = true;
         $this.locations = [];
         var callback = function(location) {
           console.log(location);
+          location.localtime = Date.now();
           $scope.$apply(function() { $this.locations.push(location); });
           backgroundGeolocation.finish();
         };
